00745e2278041d131e8a225926f2e0958a1559e2
narayanLohani <narayanlohani07@gmail.com>
2025-06-28 18:01:41 +0545

    Feat: Header Component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,11 @@
 const Header = () => {
-    return <>Simple Notes App</>;
+    return (
+        <div className="flex w-full bg-primary items-center justify-center border-b-8 border-b-dark ">
+            <div className="text-center text-light font-title">
+                Simple Notes App
+            </div>
+        </div>
+    );
 };
 
 export default Header;
