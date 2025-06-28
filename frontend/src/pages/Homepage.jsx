6e750ff1dbe65e5ba417bd912136c4b9116308ee
narayanLohani <narayanlohani07@gmail.com>
2025-06-28 18:38:59 +0545

    Feat: Fetch notes in HomePage.jsx

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Homepage = () => {
-    return <div>Homepage</div>;
+    const [notes, setNotes] = useState([]);
+
+    useEffect(() => {
+        const fetchNotes = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:5000/api/notes/"
+                );
+                setNotes(response.data);
+            } catch (err) {
+                console.error("Error fetching notes: ", err);
+            }
+        };
+        fetchNotes();
+    }, []);
+
+    return (
+        <div>
+            {notes.map((note) => {
+                <NoteCard note={note} key={note.id} />;
+            })}
+        </div>
+    );
 };
 
 export default Homepage;
