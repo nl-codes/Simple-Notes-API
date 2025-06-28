176dc00b75e8f00187816aa9a9f115b3b112657e
narayanLohani <narayanlohani07@gmail.com>
2025-06-28 18:05:58 +0545

    Feat: Footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
+import Footer from "./components/Footer";
 function App() {
     return (
         <Router>
@@ -8,6 +9,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Homepage />} />
             </Routes>
+            <Footer />
         </Router>
     );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="flex w-full bg-primary items-center justify-center border-t-8 border-t-dark">
+            <div className="text-center text-light font-title font-footer">
+                Narayan Lohani &copy;
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
